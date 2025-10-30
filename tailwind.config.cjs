fa96407350ace7ee8a4c3e8f918e4819468fd7f3
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-10-30 20:53:58 +0700

    Tailwind viewer, editor, and formatter implementation

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{css,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
